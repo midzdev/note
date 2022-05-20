6e561b86ace9350b70a7370844a2aa364f255a4a
admin <wiwm07@gmail.com>
2022-05-20 16:07:28 +0800

    minor bugs fixed

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -11,20 +11,21 @@ export default function Note() {
 
   useEffect(() => {
     (async () => {
-      const { data } =
-        id && (await axios.get(`https://api.midzdev.com/note/${id}`));
-      setNote(data);
+      const res = id && (await axios.get(`https://api.midzdev.com/note/${id}`));
+      setNote(res && res.data);
     })();
   }, [id]);
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <Head>
-        <meta property="og:title" content={`Note ID: ${id && id}`} />
-      </Head>
-      <code className="bg-neutral-800 border border-neutral-700 rounded-md p-2 w-[1024px] text-white focus:border-indigo-500 duration-200 font-['Fira_Code'] text-sm">
-        {note}
-      </code>
-    </div>
+    id && (
+      <div className="flex flex-col justify-center items-center h-screen">
+        <Head>
+          <meta property="og:title" content={`Note ID: ${id && id}`} />
+        </Head>
+        <code className="bg-neutral-800 border border-neutral-700 rounded-md p-2 w-[1024px] text-white focus:border-indigo-500 duration-200 font-['Fira_Code'] text-sm">
+          {note}
+        </code>
+      </div>
+    )
   );
 }
