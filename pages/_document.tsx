a02af18515037a97ae97374d647b7c04f20cd48a
admin <wiwm07@gmail.com>
2022-05-31 13:58:34 +0800

    removed extra font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
           crossOrigin="true"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;600&family=Inter:wght@400;600;800&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400&display=swap"
           rel="stylesheet"
         />
       </Head>
